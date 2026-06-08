394f0be5dbe4dfbab0fcd333689bdbdfd86befb3
Henrique Barros <santoshenrique0319@gmail.com>
2026-06-07 23:11:07 -0300

    Enhance handler to support POST requests

--- api/teste.js
+++ api/teste.js
@@ -1,5 +1,16 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    status: "ok"
-  });
+export default async function handler(req, res) {
+
+    if (req.method === "POST") {
+
+        const dados = req.body;
+
+        return res.status(200).json({
+            mensagem: "Recebi os dados",
+            dados
+        });
+    }
+
+    res.status(200).json({
+        status: "Backend funcionando"
+    });
 }
