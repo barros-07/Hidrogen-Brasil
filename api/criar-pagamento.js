f4bb036a73839fe857692c349da069451ef1ca3a
Henrique Barros <santoshenrique0319@gmail.com>
2026-06-08 01:27:26 -0300

    Make handler function asynchronous and log product
    Updated handler function to be asynchronous and log received product.

--- api/criar-pagamento.js
+++ api/criar-pagamento.js
@@ -1,4 +1,9 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
+
+    const { produto } = req.body;
+
+    console.log("Produto recebido:", produto);
+
     res.status(200).json({
         checkout: "https://google.com"
     });
